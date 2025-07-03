ec55dc4316bb7670e43bfaca1b4f18deb980c4dd
Najib Bouabdallah <119747481+NajibBouabdallah@users.noreply.github.com>
2025-07-03 00:46:14 +0000

    Update navbar.tsx

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { motion } from "framer-motion";
-import { Button } from "./ui/button";
+
 import Link from "next/link";
 import { useCartStore } from "@/store/cart-store";
 import { ShoppingCart } from "lucide-react";
